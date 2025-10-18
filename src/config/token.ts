6d00fcd5ec482cf90dfd9cec1d80758facf5e1cd
!skhak <nurlanjunushaliev76@icloud.com>
2025-10-18 17:50:44 +0600

    token-"dotenv.config();"

--- src/config/token.ts
+++ src/config/token.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import jwt from "jsonwebtoken";
+
 const JWT_SECRET = process.env.JWT_SECRET!;
 export const generateToken = (userId: string, userEmail: string) => {
   return jwt.sign({ id: userId, email: userEmail }, JWT_SECRET, {
