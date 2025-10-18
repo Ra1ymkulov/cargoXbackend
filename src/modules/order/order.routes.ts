e2ae6ec6043b1ff5ea482b425c5c8e1a0781d4d8
!skhak <nurlanjunushaliev76@icloud.com>
2025-10-18 19:40:47 +0600

    iskhak/notification/function/backend

--- src/modules/order/order.routes.ts
+++ src/modules/order/order.routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post("/create-order", orderControllers.createOrder);
 router.get("/get-all-order", orderControllers.getAllOrder);
 router.put("/user/:id/read", orderControllers.readOrder);
+router.post("/notification-status", orderControllers.notificationFunc);
 
 export default router;
