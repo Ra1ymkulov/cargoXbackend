0e60d3213790b9aade4ad9d560656091f3c29621
!skhak <nurlanjunushaliev76@icloud.com>
2025-10-21 18:23:34 +0600

    dev-tg

--- src/config/telegram.ts
+++ src/config/telegram.ts
@@ -1,18 +1,26 @@
 import axios from "axios";
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const CHAT_ID = process.env.TELEGRAM_CHAT_ID; // можно оставить, если хочешь дефолтную группу
-const GROUP_ID = process.env.GROUP_ID;
 
-const sendTelegramMessage = async (text: string, groupId?: number) => {
-  try {
-    await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
-      chat_id: GROUP_ID || CHAT_ID,
-      text,
-      parse_mode: "HTML",
-    });
-  } catch (e: any) {
-    console.error("Ошибка отправки в Telegram:", e.response?.data || e.message);
+const CHAT_IDS = ["968101120", "5124900056", "6955955270"];
+const sendTelegramMessage = async (text: string) => {
+  for (const chatId of CHAT_IDS) {
+    try {
+      const response = await axios.post(
+        `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+        {
+          chat_id: chatId,
+          text,
+          parse_mode: "HTML",
+        }
+      );
+      console.log(`Сообщение отправлено в чат ${chatId}:`, response.data);
+    } catch (e: any) {
+      console.error(
+        `Ошибка отправки в чат ${chatId}:`,
+        e.response?.data || e.message
+      );
+    }
   }
 };
 
