9f6498b366f623904b10f1d47691ec49ee2a63f8
kyuiazbek <kyuiazbekchoibekov@gmail.com>
2025-10-23 19:21:16 +0600

    dev

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -7,7 +7,7 @@ router.get("/get-user/:id", userControllers.getUser);
 router.get("/get-all-users", userControllers.getAllUser);
 router.get("/get-all-service", userControllers.getAllService);
 router.get("/get-service-type", userControllers.getServiceType);
-router.get(
+router.post(
   "/message-contact-telegram",
   userControllers.TelegramBotContactMessage
 );
