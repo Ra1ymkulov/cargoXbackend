5c15320dce2af736d74afb53a5a4c46971e745b2
Ra1ymkulov <retype.uun@gmail.com>
2025-10-23 15:31:42 +0600

    dev

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,9 @@ import { Router } from "express";
 import authRouter from "../modules/auth/auth.routes";
 import orderRouter from "../modules/order/order.routes";
 import userRouter from "../modules/user/user.routes";
-import cors from "cors";
 const router = Router();
 
-const corsConfig = {
-  origin: ["http://localhost:3000"],
-};
-
-router.use("/auth", cors(corsConfig), authRouter);
+router.use("/auth", authRouter);
 router.use("/order", orderRouter);
 router.use("/user", userRouter);
 
