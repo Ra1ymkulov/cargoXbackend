d3f680e8351606a6e67babdd43449f616c64fc7e
!skhak <nurlanjunushaliev76@icloud.com>
2025-10-30 14:47:44 +0600

    iskhak-status/api

--- src/modules/order/order.routes.ts
+++ src/modules/order/order.routes.ts
@@ -6,6 +6,6 @@ const router = Router();
 router.post("/create-order", orderControllers.createOrder);
 router.get("/get-all-order", orderControllers.getAllOrder);
 router.put("/user/:id/read", orderControllers.readOrder);
-router.post("/status-change/:orderId/:status", orderControllers.statusFunc);
+router.post("/status-change/:orderId", orderControllers.statusFunc);
 
 export default router;
