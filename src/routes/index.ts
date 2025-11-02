13882ae807dfc6fcce9d58bbec5634b5f0035c1a
!skhak <nurlanjunushaliev76@icloud.com>
2025-11-02 15:23:43 +0600

    iskhak-deleteOrder

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import authRouter from "../modules/auth/auth.routes";
 import orderRouter from "../modules/order/order.routes";
 import userRouter from "../modules/user/user.routes";
-import reviewsRouter from "../modules/reviews/reviews.routes";
+// import reviewsRouter from "../modules/reviews/reviews.routes";
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/order", orderRouter);
 router.use("/user", userRouter);
-router.use("/reviews", reviewsRouter);
+// router.use("/reviews", reviewsRouter);
 
 export default router;
